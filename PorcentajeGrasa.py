8c7fdcfcc7106843180cb744346f468af1e0d12b
Lucio Sanfilippo <67439801+luciosanfi@users.noreply.github.com>
2025-06-22 22:24:01 -0300

    Update PorcentajeGrasa.py
    El print no es necesario, fue solo para probar la funcion mientras la definía

--- PorcentajeGrasa.py
+++ PorcentajeGrasa.py
@@ -1,7 +1,6 @@
-import funciones1
+import IMC
 def porcentaje_grasa(peso,altura,edad,valor_genero):
-    imc = funciones1.calcular_imc(peso,altura)
+    imc = IMC.calcular_imc(peso,altura)
     porcentaje = 1.2 * imc + 0.23 *edad - 5.4 - valor_genero
     return porcentaje
-print("El porcentaje de grasa corporal es de:\n",porcentaje_grasa(100,1.70,30,0))
 
